907f20ae899310604d12765021e268a9c615f47b
novruzoff9 <yagmurnov2005@gmail.com>
2024-05-23 13:53:06 +0400

    Added SwitchCase Operations

---
+++ SwitchCase/SwitchCaseExamples/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace SwitchCaseExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string season = Console.ReadLine();
+            Console.WriteLine("Switch Case Ile");
+            WhichSeason(season);
+
+            Console.WriteLine("Sade Switch Case Ile");
+            WhichSeasonExp(season);
+
+            Console.WriteLine("TermaryIf ile");
+            Termary(season);
+        }
+
+        static void WhichSeason(string season)
+        {
+            string result = "";
+            switch (season)
+            {
+                case "Spring":
+                    result = "Yaz";
+                    break;
+                case "Summer":
+                    result = "Yay";
+                    break;
+                case "Autmn":
+                    result = "Payiz";
+                    break;
+                case "Winter":
+                    result = "Qis";
+                    break;
+            }
+            Console.WriteLine(result);
+        }
+
+        static void WhichSeasonExp(string season)
+        {
+            string result = "";
+            string res = season switch
+            {
+                "Spring" => "Yaz",
+                "Summer" => "Yay",
+                "Autmn" => "Payiz",
+                "Winter" => "Qis",
+                _ => "Daxiletme duzgun deyil"
+            };
+
+
+            switch (season)
+            {
+                case "Spring":
+                    result = "Yaz";
+                    break;
+                case "Summer":
+                    result = "Yay";
+                    break;
+                case "Autmn":
+                    result = "Payiz";
+                    break;
+                case "Winter":
+                    result = "Qis";
+                    break;
+                default:
+                    result = "Daxiletme duzgun deyil";
+                    break;
+            }
+            Console.WriteLine(result);
+            Console.WriteLine(res);
+        }
+
+        static void Termary(string season)
+        {
+            string result = season == "Spring" ? "Yaz" : season == "Summer" ? "Yay" : season == "Autmn" ?
+                "Payiz" : season == "Winter" ? "Qis" : "Daxiletme yanlisdir";
+            Console.WriteLine(result);
+        }
+    }
+}
