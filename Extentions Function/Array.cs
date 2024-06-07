f3a818a4145ca908e6cee63e6f4dbfad219d70b2
novruzoff9 <yagmurnov2005@gmail.com>
2024-06-07 22:27:57 +0400

    Added Extension Function for Array

---
+++ Extentions Function/Array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public class Array
+    {
+        /// <summary>
+        /// Dizinin sonuna istediğiniz değerleri ekleyin
+        /// </summary>
+        /// <param name="array">Şu anki dizin</param>
+        /// <param name="values">Artrırılacak olan değerler</param>
+        public static void MyResize<T>(ref T[] array, params T[] values)
+        {
+            T[] newarray = new T[array.Length + values.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                newarray[i] = array[i];
+            }
+            for (int i = 0; i < values.Length; i++)
+            {
+                newarray[array.Length + i] = values[i];
+            }
+
+            for (int i = 0; i < newarray.Length; i++)
+            {
+                Console.Write(newarray[i] + " ");
+            }
+        }
+    }
+}
---
+++ Extentions Function/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Extentions_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Int veri tipinde örnek
+            int[] array = new int[] { 1, 2, 3 };
+            System.Array.MyResize(ref array, 4, 2, 5, 7, 6);
+
+            Console.WriteLine();
+            //String veri tipinde örnek
+            string[] arraystr = new string[] { "oncedeger1", "oncedeger2", "oncedeger3" };
+            System.Array.MyResize(ref arraystr, "artirilandeger1", "artirilandeger2");
+        }
+    }
+}
